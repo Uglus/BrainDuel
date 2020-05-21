84a5b7ea49622fa3f82faba3b9b2f9ab4687f777
Husein Janien <huseinjanien@gmail.com>
2020-05-21 10:45:32 +0300

    first commit

---
+++ Windows/ViewModels/ItemMenu.cs
@@ -0,0 +1,29 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Windows.ViewModels
+{
+    public class ItemMenu
+    {
+        public ItemMenu(string header, List<SubItem> subItems, PackIconKind icon)
+        {
+
+        }
+        public ItemMenu(string header, UserControl screen, PackIconKind icon)
+        {
+
+        }
+
+        public string Header { get; private set; }
+        public PackIcon Icon { get; private set; }
+        public List<SubItem> SubItems { get; private set; }
+
+
+
+    }
+}
